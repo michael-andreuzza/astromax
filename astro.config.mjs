10c9c601a546a32fd00a7efab7569bb85e231a84
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2023-08-30 16:51:15 +0300

    first push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import mdx from "@astrojs/mdx"; 
 export default defineConfig({
   site: 'https://lexingtonthemes.com',
   integrations: [tailwind(), sitemap(), ]
