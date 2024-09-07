d3c661030522f1d243e7d8ffd803d9f232450388
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-09-08 00:20:57 +0300

    Update: Breaking changes: Added Tailwind CSS V4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
+   vite: {
+    plugins: [tailwindcss()],
+  },
+  // add yur domain name here
   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(), sitemap(), ]
+  compressHTML: true,
+  integrations: [sitemap()]
 });
